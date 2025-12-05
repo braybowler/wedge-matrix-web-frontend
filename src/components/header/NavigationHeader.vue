be14f428a8d5ede46b40de582a0215cac3624bab
braybowler <braybowler1995@gmail.com>
2025-12-05 08:06:51 -0500

    make app header a separate component

---
+++ src/components/header/NavigationHeader.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="header">
+    <nav class="navigation-container">
+      <RouterLink active-class="active-link" class="link" to="/matrix"> Wedge Matrix </RouterLink>
+      <RouterLink active-class="active-link" class="link" to="/configure"> Configure </RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  border-bottom: 1px solid #4b5563;
+}
+
+.navigation-container {
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-around;
+  padding: 16px;
+}
+
+.link {
+  color: #9ca3af;
+}
+
+.active-link {
+  color: #818cf8;
+}
+</style>
