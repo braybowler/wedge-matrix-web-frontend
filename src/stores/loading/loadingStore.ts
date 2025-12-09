25a286e2d75aadb4578e408706eaec4f284a4a1b
braybowler <braybowler1995@gmail.com>
2025-12-09 08:50:51 -0500

    env variables for basepath, axios composable, loading store

--- src/stores/loading/loadingStore.ts
+++ src/stores/loading/loadingStore.ts
@@ -1,37 +1,10 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
-import type {
-  AllowableMatrixColumnNumber,
-  RowDisplayOption,
-  UserColumnHeader,
-} from '@/types/matrix'
-export const useMatrixConfigurationStore = defineStore('matrixConfiguration', () => {
-  const matrixColumns = ref<AllowableMatrixColumnNumber>(4)
-  const matrixColumnHeaders = ref<Array<UserColumnHeader>>([
-    {
-      label: '25%',
-      id: 1,
-    },
-    {
-      label: '50%',
-      id: 2,
-    },
-    {
-      label: '75%',
-      id: 3,
-    },
-    {
-      label: '100%',
-      id: 4,
-    },
-  ])
-  const selectedRowDisplayOption = ref<RowDisplayOption>('Carry')
-  const matrixRowDisplayOptions = ref<Array<RowDisplayOption>>(['Carry', 'Total', 'Both'])
+
+export const useLoadingStore = defineStore('loading', () => {
+  const isLoading = ref(false);
 
   return {
-    matrixColumns,
-    matrixColumnHeaders,
-    matrixRowDisplayOptions,
-    selectedRowDisplayOption,
+    isLoading
   }
 })
